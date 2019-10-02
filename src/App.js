6c72e9248b68eec1cdeadb5e280604af11659863
aushvinvasanth <aushvin23@gmail.com>
2019-10-02 15:01:08 -0400

    Restaurant API call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, {Component} from 'react';
 import './App.css';
 
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      userInput: '',
+      userInput: 'chinese',
     }
   }
 
-  
-
+restaurantList() {
+  Axios ({
+    method: 'get',
+    url: `https://developers.zomato.com/api/v2.1/search?`,
+    responseType: 'json',
+    headers: {
+      'user-key': '2b0993e0c699ef3fdaa848e535d9df9f',
+      'Accept': 'application / json'
+    },
+    params: {
+      entity_id: 89,
+      entity_type: 'city',
+      start: searchStart,
+      sort: 'rating',
+      order: 'desc',
+      q: this.state.userInput
+      //API Call directed @ User Input
+    }
+  }).then((res) => {
+    const originalResults = res.data.restaurants.filter((item) => {
+      return item.restaurant.user_rating.votes > 0
+    })
+    // returning an object that holds information about each restaurant
+    const restaurantResults = originalResults.map((item) => {
+      return {
+        name: item.restaurant.name,
+        thumb: item.restaurant.featured_image,
+        address: item.restaurant.location.address,
+        rating: item.restaurant.user_rating.aggregate_rating,
+        votes: item.restaurant.user_rating.votes,
+        cost: item.restaurant.price_range,
+        phone: item.restaurant.phone_numbers,
+      }
+  })
+})}
 
   render() {
     return (
